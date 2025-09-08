a1aabb3187fa1a5a321eeb39ac1a6f382c8a30aa
mrichetto-rula <mattia.richetto@rula.com>
2025-09-08 10:48:14 -0400

    my solution WIP

--- src/GameOfTennis.test.ts
+++ src/GameOfTennis.test.ts
@@ -1,6 +1,62 @@
 import { GameOfTennis } from './GameOfTennis';
 
-test('GameOfTennis is not null', () => {
+test('The score is Love-Love', () => {
   const game = new GameOfTennis();
-  expect(game).not.toBe(null);
+  expect(game.score()).toEqual('Love-Love');
+});
+
+test('When Player 1 gets the point, then the score is 15-Love', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 1');
+  expect(game.score()).toEqual('15-Love');
+});
+
+test('When Player 2 gets the point, then the score is Love-15', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 2');
+  expect(game.score()).toEqual('Love-15');
+});
+
+test('P1 P1, then the score is 30-Love', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 1');
+  game.pointTo('Player 1');
+  expect(game.score()).toEqual('30-Love');
+});
+
+test('P1 P2 P1, then the score is 30-15', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 1');
+  game.pointTo('Player 2');
+  game.pointTo('Player 1');
+  expect(game.score()).toEqual('30-15');
+});
+
+test('P1 P1 P1 P1, then the score is Player 1 wins', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 1');
+  game.pointTo('Player 1');
+  game.pointTo('Player 1');
+  game.pointTo('Player 1');
+  expect(game.score()).toEqual('Player 1 wins');
+});
+
+test('P2 P2 P2 P2, then the score is Player 2 wins', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 2');
+  game.pointTo('Player 2');
+  game.pointTo('Player 2');
+  game.pointTo('Player 2');
+  expect(game.score()).toEqual('Player 2 wins');
+});
+
+test('P1 P2 P1 P2 P1 P2, then the score is Deuce', () => {
+  const game = new GameOfTennis();
+  game.pointTo('Player 1');
+  game.pointTo('Player 2');
+  game.pointTo('Player 1');
+  game.pointTo('Player 2');
+  game.pointTo('Player 1');
+  game.pointTo('Player 2');
+  expect(game.score()).toEqual('Deuce');
 });
--- src/GameOfTennis.ts
+++ src/GameOfTennis.ts
@@ -1,1 +1,32 @@
-export class GameOfTennis {}
+const PLAYER_1: string = 'Player 1';
+const PLAYER_2: string = 'Player 2';
+
+type Player = typeof PLAYER_1 | typeof PLAYER_2;
+
+export class GameOfTennis {
+  _points = ['Love', '15', '30', '40'];
+  _p1 = 0;
+  _p2 = 0;
+
+  pointTo(player: Player): void {
+    if (player === PLAYER_1) {
+      this._p1 += 1;
+    } else {
+      this._p2 += 1;
+    }
+  }
+
+  score(): string {
+    if (this._p1 === 4) {
+      return `${PLAYER_1} wins`;
+    }
+    if (this._p2 === 4) {
+      return `${PLAYER_2} wins`;
+    }
+    if (this._p1 === 3 && this._p2 === 3) {
+      return 'Deuce';
+    }
+
+    return `${this._points[this._p1]}-${this._points[this._p2]}`;
+  }
+}
